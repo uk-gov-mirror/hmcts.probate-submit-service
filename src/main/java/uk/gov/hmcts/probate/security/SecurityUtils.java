87355badaa0bc50359fd632940a52e3bc37fdce1
venumadhavancha <37374278+venumadhavancha@users.noreply.github.com>
2018-06-06 15:28:15 +0100

    add log message with token (#31)
    * add log message with token
    
    * corrected log class

--- src/main/java/uk/gov/hmcts/probate/security/SecurityUtils.java
+++ src/main/java/uk/gov/hmcts/probate/security/SecurityUtils.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.probate.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import uk.gov.hmcts.probate.services.submit.clients.CoreCaseDataMapper;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class SecurityUtils {
 
     private final AuthTokenGenerator authTokenGenerator;
+    private final Logger logger = LoggerFactory.getLogger(SecurityUtils.class);
 
     @Autowired
     public SecurityUtils(final AuthTokenGenerator authTokenGenerator) {
@@ -18,7 +22,9 @@ public class SecurityUtils {
 
     public HttpHeaders authorizationHeaders() {
         final HttpHeaders headers = new HttpHeaders();
-        headers.add("ServiceAuthorization", authTokenGenerator.generate());
+        String serviceAuthToken = authTokenGenerator.generate();
+        headers.add("ServiceAuthorization", serviceAuthToken);
+        logger.info("serviceAuthtoken Generated...." + serviceAuthToken);
         return headers;
     }
 }

04289af46be5fe663c5b796769fdf474f222779e
Venu Ancha <vancha@hmcts.net>
2018-06-06 17:15:56 +0100

    remove logging

--- src/main/java/uk/gov/hmcts/probate/security/SecurityUtils.java
+++ src/main/java/uk/gov/hmcts/probate/security/SecurityUtils.java
@@ -1,11 +1,7 @@
 package uk.gov.hmcts.probate.security;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import uk.gov.hmcts.probate.services.submit.clients.CoreCaseDataMapper;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +9,6 @@ import org.springframework.stereotype.Component;
 public class SecurityUtils {
 
     private final AuthTokenGenerator authTokenGenerator;
-    private final Logger logger = LoggerFactory.getLogger(SecurityUtils.class);
 
     @Autowired
     public SecurityUtils(final AuthTokenGenerator authTokenGenerator) {
@@ -22,9 +17,7 @@ public class SecurityUtils {
 
     public HttpHeaders authorizationHeaders() {
         final HttpHeaders headers = new HttpHeaders();
-        String serviceAuthToken = authTokenGenerator.generate();
-        headers.add("ServiceAuthorization", serviceAuthToken);
-        logger.info("serviceAuthtoken Generated...." + serviceAuthToken);
+        headers.add("ServiceAuthorization", authTokenGenerator.generate());
         return headers;
     }
 }

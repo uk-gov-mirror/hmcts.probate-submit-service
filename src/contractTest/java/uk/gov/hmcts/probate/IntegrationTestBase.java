917a3c0bbd9ee08eda8766a9efff148dcdaeb430
Dhruv Patel <dhruv.patel20@gmail.com>
2018-06-08 14:11:37 +0100

    PRO-3048: Added contract tests.

---
+++ src/contractTest/java/uk/gov/hmcts/probate/IntegrationTestBase.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.probate;
+
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import net.thucydides.junit.spring.SpringIntegration;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.probate.util.ContractTestUtils;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestContextConfiguration.class)
+public abstract class IntegrationTestBase {
+
+    @Autowired
+    protected SolCcdServiceAuthTokenGenerator solCcdServiceAuthTokenGenerator;
+
+    @Autowired
+    protected ContractTestUtils contractTestUtils;
+
+    private String solCcdServiceUrl;
+
+    @Autowired
+    public void solCcdServiceUrl(@Value("${ccd.data.store.api.url}") String solCcdServiceUrl) {
+        this.solCcdServiceUrl = solCcdServiceUrl;
+        RestAssured.baseURI = solCcdServiceUrl;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+
+    @Rule
+    public SpringIntegration springIntegration;
+
+    public IntegrationTestBase() {
+        this.springIntegration = new SpringIntegration();
+
+    }
+}
---
+++ src/contractTest/java/uk/gov/hmcts/probate/util/ContractTestUtils.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.probate.util;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.util.ResourceUtils;
+import uk.gov.hmcts.probate.SolCcdServiceAuthTokenGenerator;
+import uk.gov.hmcts.probate.TestContextConfiguration;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+
+@ContextConfiguration(classes = TestContextConfiguration.class)
+@Component
+public class ContractTestUtils {
+
+    @Autowired
+    protected SolCcdServiceAuthTokenGenerator solCcdServiceAuthTokenGenerator;
+
+    private String serviceToken;
+
+
+    @PostConstruct
+    public void init() {
+        serviceToken = solCcdServiceAuthTokenGenerator.generateServiceToken();
+    }
+
+    public String getJsonFromFile(String fileName) {
+        try {
+            File file = ResourceUtils.getFile(this.getClass().getResource("/json/" + fileName));
+            return new String(Files.readAllBytes(file.toPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Headers getHeaders() {
+        return getHeaders(serviceToken);
+    }
+
+    public Headers getHeaders(String serviceToken) {
+        return Headers.headers(
+                new Header("ServiceAuthorization", serviceToken),
+                new Header("Content-Type", ContentType.JSON.toString()));
+    }
+
+    public Headers getHeadersWithUserId() {
+        return getHeadersWithUserId(serviceToken);
+    }
+
+    public Headers getHeadersWithUserId(String serviceToken) {
+        System.out.println("Service Token Generated..." + serviceToken);
+        return Headers.headers(
+                new Header("ServiceAuthorization", serviceToken),
+                new Header("Content-Type", ContentType.JSON.toString()),
+                new Header("Authorization", solCcdServiceAuthTokenGenerator.generateUserTokenWithNoRoles()));
+
+
+    }
+
+    public String getUserId() {
+        return solCcdServiceAuthTokenGenerator.getUserId();
+    }
+
+
+
+}

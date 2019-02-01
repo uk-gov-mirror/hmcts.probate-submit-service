a04a1133f81dc1c0736108fff999db33519518ba
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-02-01 16:02:32 +0000

    PRO-141: organise imports on all source

--- src/main/java/uk/gov/hmcts/probate/services/submit/controllers/SubmitController.java
+++ src/main/java/uk/gov/hmcts/probate/services/submit/controllers/SubmitController.java
@@ -4,7 +4,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.probate.services.submit.model.SubmitData;
 import uk.gov.hmcts.probate.services.submit.services.SubmitService;
 

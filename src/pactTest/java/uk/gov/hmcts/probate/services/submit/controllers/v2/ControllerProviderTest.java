09aa80e3ea736c7116fca7b45aa0418b37fcdb56
ruban72 <ruban72@hotmail.com>
2019-02-07 15:36:07 +0000

    Refactor tests

--- src/pactTest/java/uk/gov/hmcts/probate/services/submit/controllers/v2/ControllerProviderTest.java
+++ src/pactTest/java/uk/gov/hmcts/probate/services/submit/controllers/v2/ControllerProviderTest.java
@@ -1,20 +1,10 @@
 package uk.gov.hmcts.probate.services.submit.controllers.v2;
 
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.loader.PactBroker;
-import au.com.dius.pact.provider.junit.target.HttpTarget;
-import au.com.dius.pact.provider.junit.target.Target;
-import au.com.dius.pact.provider.junit.target.TestTarget;
-import au.com.dius.pact.provider.spring.SpringRestPactRunner;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.ResourceUtils;
 import uk.gov.hmcts.reform.probate.model.cases.ProbateCaseDetails;
 
@@ -23,20 +13,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 
-@RunWith(SpringRestPactRunner.class)
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {
-        "server.port=8123", "spring.application.name=PACT_TEST"
-})
-@PactBroker(host = "${pact.broker.baseUrl}", port = "${pact.broker.port}")
+
 abstract public class ControllerProviderTest {
 
     @Autowired
     ObjectMapper objectMapper;
-
-    @TestTarget
-    @SuppressWarnings(value = "VisibilityModifier")
-    public final Target target = new HttpTarget("http", "localhost", 8123, "/");
 
     @Before
     public void setUpTest() {

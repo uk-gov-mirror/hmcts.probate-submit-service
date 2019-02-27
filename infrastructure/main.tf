4a0742c6614a554310f57a0ad615bea97a9b2480
reddy1praveen <32567752+reddy1praveen@users.noreply.github.com>
2019-02-27 23:24:58 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,12 +1,12 @@
-provider "vault" {
+#   //  # provider "vault" {
   //  # It is strongly recommended to configure this provider through the
   //  # environment variables described above, so that each user can have
   //  # separate credentials set in the environment.
   //  #
   //  # This will default to using $VAULT_ADDR
   //  # But can be set explicitly
-  address = "https://vault.reform.hmcts.net:6200"
-}
+#  //  #   address = "https://vault.reform.hmcts.net:6200"
+#  //  # }
 
 
 # data "vault_generic_secret" "probate_mail_host" {

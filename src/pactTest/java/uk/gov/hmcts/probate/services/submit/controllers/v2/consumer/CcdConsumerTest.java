fe69897e076f55e76cfd24a7feb0f1ef9e8fd0fb
ruban72 <ruban72@hotmail.com>
2020-10-19 08:35:23 +0100

    TA-25: Refactor CCD pact dsl

--- src/pactTest/java/uk/gov/hmcts/probate/services/submit/controllers/v2/consumer/CcdConsumerTest.java
+++ src/pactTest/java/uk/gov/hmcts/probate/services/submit/controllers/v2/consumer/CcdConsumerTest.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.probate.services.submit.controllers.v2.consumer;
 
+import static io.pactfoundation.consumer.dsl.LambdaDsl.newJsonBody;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import au.com.dius.pact.consumer.Pact;
-import au.com.dius.pact.consumer.dsl.DslPart;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.model.RequestResponsePact;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.apache.http.client.fluent.Executor;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,14 +37,6 @@ import uk.gov.hmcts.reform.probate.model.cases.CaseType;
 import uk.gov.hmcts.reform.probate.model.cases.EventId;
 import uk.gov.hmcts.reform.probate.model.cases.JurisdictionId;
 import uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.GrantOfRepresentationData;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import static io.pactfoundation.consumer.dsl.LambdaDsl.newJsonBody;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
 
 
 @ExtendWith(PactConsumerTestExt.class)
@@ -91,7 +89,7 @@ public class CcdConsumerTest {
         Executor.closeIdleConnections();
     }
 
-    @Pact(state = "GrantOfRepresentation Case 654321 exists", provider = "ccd", consumer = "probate_submitService")
+    @Pact(state = "GrantOfRepresentation Case 654321 exists", provider = "ccd", consumer = "probate_submit_service")
     RequestResponsePact getCaseById(PactDslWithProvider builder) {
         // @formatter:off
         return builder
@@ -104,12 +102,12 @@ public class CcdConsumerTest {
                 .willRespondWith()
                 .matchHeader(HttpHeaders.CONTENT_TYPE, "\\w+\\/[-+.\\w]+;charset=(utf|UTF)-8")
                 .status(200)
-                .body(buildCaseDataPactDsl("someEmailAddress.com", true))
+                .body(CaseDataPactDslBuilder.build(CASE_ID, "someEmailAddress.com", true, true))
                 .toPact();
     }
 
 
-    @Pact(state = "Start event for citizen", provider = "ccd", consumer = "probate_submitService")
+    @Pact(state = "Start event for citizen", provider = "ccd", consumer = "probate_submit_service")
     RequestResponsePact startEventForCitizen(PactDslWithProvider builder) {
 
         // @formatter:off
@@ -136,7 +134,7 @@ public class CcdConsumerTest {
                 .toPact();
     }
 
-    @Pact(state = "Submit event for citizen", provider = "ccd", consumer = "probate_submitService")
+    @Pact(state = "Submit event for citizen", provider = "ccd", consumer = "probate_submit_service")
     RequestResponsePact submitEventForCitizen(PactDslWithProvider builder) throws IOException, JSONException {
         // @formatter:off
         return builder
@@ -188,93 +186,6 @@ public class CcdConsumerTest {
                 CaseType.GRANT_OF_REPRESENTATION.getName(), CASE_ID.toString(), Boolean.TRUE, caseDataContent);
         assertThat(caseDetails.getId(), equalTo(CASE_ID));
 
-    }
-
-    private DslPart buildCaseDataPactDsl(String someemailaddressHostCom, boolean withExecutors) {
-        return newJsonBody((o) -> {
-            o.stringType("id", CASE_ID.toString())
-                    .stringMatcher("state", "Draft|PaAppCreated|CaseCreated", "Draft")
-                    .stringValue("case_type", CaseType.GRANT_OF_REPRESENTATION.name())
-                    .object("data", (cd) -> {
-                                cd.stringValue("type", CaseType.GRANT_OF_REPRESENTATION.getName())
-                                        .stringValue("applicationType", "Personal")
-                                        .stringType("primaryApplicantForenames", "Jon")
-                                        .stringType("primaryApplicantSurname", "Snow")
-                                        .stringMatcher("primaryApplicantAddressFound",
-                                                "Yes|No", "Yes")
-                                        .stringMatcher("primaryApplicantPhoneNumber", "[0-9]+", "123455678")
-                                        .stringMatcher("primaryApplicantRelationshipToDeceased", "partner|child|sibling|partner|parent|adoptedChild|other", "adoptedChild")
-                                        .stringMatcher("primaryApplicantAdoptionInEnglandOrWales", "(Yes|No)", "Yes")
-                                        .stringValue("primaryApplicantEmailAddress", someemailaddressHostCom)
-                                        .object("primaryApplicantAddress", (address) ->
-                                                address.stringType("AddressLine1", "Pret a Manger")
-                                                        .stringType("AddressLine2", "St. Georges Hospital")
-                                                        .stringType("PostTown", "London")
-                                                        .stringType("PostCode", "SW17 0QT")
-                                        )
-                                        .stringType("deceasedForenames", "Ned")
-                                        .stringType("deceasedSurname", "Stark")
-                                        .stringMatcher("deceasedDateOfBirth", REGEX_DATE, "1930-01-01")
-                                        .stringMatcher("deceasedDateOfDeath", REGEX_DATE, "2018-01-01")
-                                        .object("deceasedAddress", (address) ->
-                                                address.stringType("AddressLine1", "Winterfell")
-                                                        .stringType("AddressLine2", "Westeros")
-                                                        .stringType("PostTown", "London")
-                                                        .stringType("PostCode", "SW17 0QT")
-                                        )
-                                        .stringMatcher("deceasedAddressFound", "Yes|No", "Yes")
-                                        .stringMatcher("deceasedAnyOtherNames", "Yes|No", "No")
-                                        .minArrayLike("deceasedAliasNameList", 0, 2,
-                                                alias -> alias
-                                                        .object("value", (value) ->
-                                                                value.stringType("Forenames", "King")
-                                                                        .stringType("LastName", "North")
-                                                        ))
-                                        .stringMatcher("deceasedMartialStatus", "marriedCivilPartnership|divorcedCivilPartnership|widowed|judicially|neverMarried")
-                                        .stringMatcher("deceasedDivorcedInEnglandOrWales", "Yes|No", "No")
-                                        .stringMatcher("deceasedSpouseNotApplyingReason", "renunciated|mentallyIncapable|other")
-                                        .stringMatcher("deceasedOtherChildren", "Yes|No", "Yes")
-                                        .stringMatcher("childrenOverEighteenSurvived", "Yes|No", "Yes")
-                                        .stringMatcher("childrenDied", "Yes|No", "No")
-                                        .stringMatcher("grandChildrenSurvivedUnderEighteen", "Yes|No", "No")
-                                        .stringMatcher("deceasedAnyChildren", "Yes|No", "No")
-                                        .stringMatcher("deceasedHasAssetsOutsideUK", "Yes|No", "Yes")
-                                        .stringMatcher("deceasedAnyChildren", "Yes|No", "No")
-                                        .stringMatcher("ihtFormId", "IHT205|IHT207|IHT400421", "IHT205")
-                                        .stringMatcher("ihtFormCompletedOnline", "Yes|No", "No")
-                                        .stringType("assetsOverseasNetValue", "10050")
-                                        .stringType("ihtGrossValue", "100000")
-                                        .stringType("ihtNetValue", "100000")
-                                        .stringType("ihtReferenceNumber", "GOT123456")
-                                        .stringMatcher("declarationCheckbox", "Yes|No", "No")
-                                        .numberType("outsideUKGrantCopies", 6)
-                                        .numberType("extraCopiesOfGrant", 3)
-                                        .stringType("uploadDocumentUrl", "http://document-management/document/12345")
-                                        .stringMatcher("registryLocation", "Oxford|Manchester|Birmingham|Leeds|Liverpool|Brighton|Cardiff|London|Winchester|Newcastle|ctsc", "Oxford");
-
-                                if (withExecutors) {
-                                    cd.minArrayLike("executorsApplying", 0, 2,
-                                            executorApplying -> executorApplying
-                                                    .object("value", (value) ->
-                                                            value.stringType("applyingExecutorName", "Jon Snow")
-                                                                    .stringMatcher("applyingExecutorPhoneNumber", "[0-9]+", "07981898999")
-                                                                    .stringMatcher("applyingExecutorAgreed", "Yes|No", "Yes")
-                                                                    .stringType("applyingExecutorEmail", "address@email.com")
-                                                                    .stringType("applyingExecutorInvitationId", "54321")
-                                                                    .stringType("applyingExecutorLeadName", "Graham Garderner")
-                                                                    .object("applyingExecutorAddress", (address) ->
-                                                                            address.stringType("AddressLine1", "Winterfell")
-                                                                                    .stringType("AddressLine2", "Westeros")
-                                                                                    .stringType("PostTown", "London")
-                                                                                    .stringType("PostCode", "SW17 0QT")
-                                                                    ).stringType("applyingExecutorOtherNames", "Graham Poll")
-                                                                    .stringType("applyingExecutorOtherNamesReason", "Divorce")
-                                                    ));
-                                }
-
-                            }
-                    );
-        }).build();
     }
 
     protected JSONObject createJsonObject(Object obj) throws JSONException, IOException {

9a49226706f593a0a2aeea19e1042f8970b14321
satya <1426759+satyachundur@users.noreply.github.com>
2021-05-05 11:51:50 +0100

    Update security.sh

--- security.sh
+++ security.sh
@@ -4,7 +4,7 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export PYTHONDONTWRITEBYTECODE=1
 
-zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -S -d -u ${SECURITY_RULES} -P 1001 -l FAIL --hook=zap_hooks.py
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SECURITY_RULES} -P 1001 -l FAIL --hook=zap_hooks.py
 cat zap.out
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o api-report.html -f html

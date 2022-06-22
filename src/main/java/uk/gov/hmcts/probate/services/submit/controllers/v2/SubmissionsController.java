dcb43c641b268ef28ffb6ece8355d27daac6c089
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2022-06-22 12:54:13 +0100

    DTSPB-2869: use springdoc

--- src/main/java/uk/gov/hmcts/probate/services/submit/controllers/v2/SubmissionsController.java
+++ src/main/java/uk/gov/hmcts/probate/services/submit/controllers/v2/SubmissionsController.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.probate.services.submit.controllers.v2;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.SwaggerDefinition;
-import io.swagger.annotations.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -21,8 +19,7 @@ import uk.gov.hmcts.reform.probate.model.cases.ProbateCaseDetails;
 import uk.gov.hmcts.reform.probate.model.cases.SubmitResult;
 
 @Slf4j
-@Api(tags = {"SubmissionsController"})
-@SwaggerDefinition(tags = {@Tag(name = "SubmissionsController", description = "Submissions API")})
+@Tag(name = "SubmissionsController", description = "Submissions API")
 @RestController
 @RequiredArgsConstructor
 public class SubmissionsController {
@@ -30,10 +27,10 @@ public class SubmissionsController {
     public static final String APPLICATION_ID = "applicationId";
     private final SubmissionsService submissionsService;
 
-    @ApiOperation(value = "Save case draft to CCD", notes = "Save case draft to CCD")
+    @Operation(summary = "Save case draft to CCD", description = "Save case draft to CCD")
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "Draft save to CCD successful"),
-        @ApiResponse(code = 400, message = "Draft save to CCD  failed")
+        @ApiResponse(responseCode = "200", description = "Draft save to CCD successful"),
+        @ApiResponse(responseCode = "400", description = "Draft save to CCD  failed")
     })
     @PostMapping(path = "/submissions/{applicationId}", consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)

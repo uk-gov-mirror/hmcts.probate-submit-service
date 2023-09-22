8212399056c496d15e8e0c9fda1b17581b8d35b1
FeliTam <feli.tam@justice.gov.uk>
2023-09-22 14:13:38 +0100

    DTSPB-3430 test SecurityConfiguration

--- src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
@@ -4,15 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Profile("!SECURITY_MOCK")
 @Configuration
@@ -22,34 +20,22 @@ public class SecurityConfiguration {
     @Autowired
     private ServiceAuthFilter serviceAuthFilter;
 
-    @Order(0)
-    @Bean
-    public SecurityFilterChain allowedList(HttpSecurity http) throws Exception {
-        return http
-            .sessionManagement(sm -> sm.sessionCreationPolicy(STATELESS))
-            .csrf(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-            .logout(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/swagger-ui.html",
-                    "/swagger-resources/**",
-                    "/swagger-ui/**",
-                    "/v3/api-docs/**",
-                    "/health",
-                    "/health/**",
-                    "/error",
-                    "/info",
-                    "/").permitAll())
-            .build();
-    }
-
-    @Order(1)
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(AbstractHttpConfigurer::disable)
             .formLogin(AbstractHttpConfigurer::disable)
             .logout(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/swagger-ui.html",
+                            "/swagger-resources/**",
+                            "/swagger-ui/**",
+                            "/v3/api-docs/**",
+                            "/health",
+                            "/health/**",
+                            "/error",
+                            "/info",
+                            "/").permitAll())
             .addFilterBefore(serviceAuthFilter, BearerTokenAuthenticationFilter.class)
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(
@@ -59,5 +45,18 @@ public class SecurityConfiguration {
                     "/ccd-case-update/**").authenticated()
                 .anyRequest().permitAll())
             .build();
+    }
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/swagger-ui.html",
+                "/swagger-resources/**",
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+                "/health",
+                "/health/**",
+                "/info",
+                "/");
     }
 }

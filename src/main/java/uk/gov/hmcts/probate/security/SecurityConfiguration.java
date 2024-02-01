c36a02895c3640569b55b2d0b1dce526aef1be29
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-02-01 14:46:04 +0000

    DTSPB-3430 update SecurityConfiguration.java for springboot3 upgrade

--- src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
@@ -3,39 +3,44 @@ package uk.gov.hmcts.probate.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
 
 @Profile("!SECURITY_MOCK")
 @Configuration
 @EnableWebSecurity
+@Order(1)
 public class SecurityConfiguration {
 
-    private ProbateServiceAuthFilter probateServiceAuthFilter;
+    private final ServiceAuthFilter serviceAuthFilter;
 
-    public SecurityConfiguration(ProbateServiceAuthFilter probateServiceAuthFilter) {
-        this.probateServiceAuthFilter = probateServiceAuthFilter;
+    public SecurityConfiguration(final ServiceAuthFilter serviceAuthFilter) {
+        this.serviceAuthFilter = serviceAuthFilter;
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-            .csrf(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-            .logout(AbstractHttpConfigurer::disable)
-            .addFilterBefore(probateServiceAuthFilter, AnonymousAuthenticationFilter.class)
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(
-                    "/cases/**",
-                    "/submissions/**",
-                    "/payments/**",
-                    "/ccd-case-update/**").permitAll()
-                .anyRequest().authenticated())
-            .build();
+    protected SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(serviceAuthFilter, BearerTokenAuthenticationFilter.class)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/cases/**",
+                                "/submissions/**",
+                                "/payments/**",
+                                "/ccd-case-update/**",
+                                "/health",
+                                "/health/liveness")
+                        .permitAll())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 
 

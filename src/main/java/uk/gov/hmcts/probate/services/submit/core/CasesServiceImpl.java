2504ef8f59897ff881db674b66109e0aa894cff2
FeliTam <feli.tam@justice.gov.uk>
2024-10-24 17:11:01 +0100

    add debug log

--- src/main/java/uk/gov/hmcts/probate/services/submit/core/CasesServiceImpl.java
+++ src/main/java/uk/gov/hmcts/probate/services/submit/core/CasesServiceImpl.java
@@ -86,6 +86,9 @@ public class CasesServiceImpl implements CasesService {
         SecurityDto securityDto = securityUtils.getSecurityDto();
         Optional<ProbateCaseDetails> caseResponseOptional = coreCaseDataService
             .findCaseById(caseId, securityDto);
+        //TODO need to remove
+        log.info("caseResponseOptional: {}",
+                caseResponseOptional.isPresent() ? caseResponseOptional.get().getCaseData() : "empty");
         return caseResponseOptional.orElseThrow(CaseNotFoundException::new);
     }
 

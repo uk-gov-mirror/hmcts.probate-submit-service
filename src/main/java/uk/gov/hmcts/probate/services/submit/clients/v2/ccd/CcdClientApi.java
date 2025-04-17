2222d27a98707936518a786fdd0ef62f8958846b
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2025-04-17 16:00:00 +0100

    DTSPB-4663 add debug log

--- src/main/java/uk/gov/hmcts/probate/services/submit/clients/v2/ccd/CcdClientApi.java
+++ src/main/java/uk/gov/hmcts/probate/services/submit/clients/v2/ccd/CcdClientApi.java
@@ -64,7 +64,7 @@ public class CcdClientApi implements CoreCaseDataService {
                 caseType.getName(), caseId, eventId.getName(), startEventResponse.getCaseDetails().getLastModified());
         if (startEventResponse.getCaseDetails().getLastModified().truncatedTo(ChronoUnit.MILLIS)
                 .isAfter(lastModifiedDateTime)) {
-            log.info("saveDraft - Saving draft for  invalid lastModifiedDateTime: {}", lastModifiedDateTime);
+            log.info("updateCase  caseId : {}, lastModifiedDateTime: {}", caseId,lastModifiedDateTime);
             throw new ConcurrentDataUpdateException(caseId);
         }
         CaseDataContent caseDataContent =
@@ -110,7 +110,7 @@ public class CcdClientApi implements CoreCaseDataService {
         );
         if (startEventResponse.getCaseDetails().getLastModified().truncatedTo(ChronoUnit.MILLIS)
                 .isAfter(lastModifiedDateTime)) {
-            log.info("saveDraft - Saving draft for  invalid lastModifiedDateTime: {}", lastModifiedDateTime);
+            log.info("updateCaseAsCaseworker caseId : {}, lastModifiedDateTime: {}", caseId,lastModifiedDateTime);
             throw new ConcurrentDataUpdateException(caseId);
         }
 

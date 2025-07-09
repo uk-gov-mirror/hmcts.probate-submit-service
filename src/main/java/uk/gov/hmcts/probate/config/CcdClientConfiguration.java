b5e7695d871ece05faaeaa74c952c31f2be5eadf
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2025-07-09 13:30:09 +0100

    DTSPB-4818 fix functional test with @Configuration

--- src/main/java/uk/gov/hmcts/probate/config/CcdClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/config/CcdClientConfiguration.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.probate.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.probate.services.submit.clients.v2.ccd.CcdClientApiErrorDecoder;
 
+@Configuration
 public class CcdClientConfiguration {
 
 
